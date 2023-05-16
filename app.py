a8b1986e74b3394470eaca3d838f7847a308f4bd
OumaymaDourrou <salamoumayma@gmail.com>
2023-05-16 09:34:32 +0200

    imm

---
+++ app.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd 
+import plotly as plt
+import streamlit as st
+import warnings
+warnings.filterwarnings('ignore')
+
+def main():
+    st.text("hello world")
+if __name__ == '__main__':
+    main()
